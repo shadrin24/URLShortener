6073a4900fafe413179a221a6ba49564d2deb4be
shadrin24 <shadrin124@gmail.com>
2025-02-15 21:40:59 +0700

    Сборка с docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:8080', // Разрешить запросы только с фронтенда
+    origin: 'http://localhost:3000', // Разрешить запросы только с фронтенда
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Если есть аутентификация
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 
 }
 bootstrap();
