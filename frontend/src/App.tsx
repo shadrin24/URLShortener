01a82da00f178ca497843164061510cdb80686b9
shadrin24 <shadrin124@gmail.com>
2025-02-19 11:11:54 +0700

    tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import ShortenUrlForm from "./Shorten/ShortenUrlForm";
 import UrlList from "./Shorten/UrlList";
 import AnalyticsPanel from "./Shorten/AnalyticsPanel";
+import "./App.css"
+
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/url";
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"], // <-- пути к твоим компонентам
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
